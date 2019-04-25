929b82bc5b023f34038d4f20c0f0eb34d6981ec3
Roderik van der Veer <roderik@settlemint.com>
2019-04-25 20:59:07 +0200

    feat: add the replicascript to the container

---
+++ replicaset.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+mongodb1=`getent hosts ${MONGO1:-"mongo"} | awk '{ print $1 }'`
+
+port=${PORT:-27017}
+
+echo "Waiting for mongo (at host $mongodb1) to start up.."
+until mongo --host ${mongodb1}:${port} --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)' &>/dev/null; do
+  printf '.'
+  sleep 1
+done
+echo "Done"
+
+echo replicaset.sh time now: `date +"%T" `
+mongo --host ${mongodb1}:${port} <<EOF
+  var cfg = {
+        "_id": "${RS:-"replicaset"}",
+        "protocolVersion": 1,
+        "members": [
+            {
+                "_id": 0,
+                "host": "${mongodb1}:${port}"
+            }
+        ]
+    };
+    rs.initiate(cfg, { force: true });
+    rs.reconfig(cfg, { force: true });
+    rs.slaveOk();
+EOF
