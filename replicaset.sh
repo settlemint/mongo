a7c29592eb9b283c8c3ef923d2211bfe9ddb532f
Roderik van der Veer <roderik@settlemint.com>
2021-01-15 12:47:23 +0100

    fix: revert

--- replicaset.sh
+++ replicaset.sh
@@ -1,29 +1,7 @@
 #!/bin/bash
 
-get_machine_ip() {
-    local -a ip_addresses
-    local hostname
-    hostname="$(hostname)"
-    read -r -a ip_addresses <<< "$(dns_lookup "$hostname" | xargs echo)"
-    if [[ "${#ip_addresses[@]}" -gt 1 ]]; then
-        warn "Found more than one IP address associated to hostname ${hostname}: ${ip_addresses[*]}, will use ${ip_addresses[0]}"
-    elif [[ "${#ip_addresses[@]}" -lt 1 ]]; then
-        error "Could not find any IP address associated to hostname ${hostname}"
-        exit 1
-    fi
-    echo "${ip_addresses[0]}"
-}
-
-get_mongo_hostname() {
-    if [[ -n "$MONGODB_ADVERTISED_HOSTNAME" ]]; then
-        echo "$MONGODB_ADVERTISED_HOSTNAME"
-    else
-        get_machine_ip
-    fi
-}
-
 mongodb1=`getent hosts ${MONGO1:-"mongo"} | awk '{ print $1 }'`
-externalMongo=`${MONGODB_ADVERTISED_HOSTNAME:-mongodb1}`
+
 port=${PORT:-27017}
 
 echo "Waiting for mongo (at host $mongodb1) to start up.."
@@ -41,7 +19,7 @@ mongo --host ${mongodb1}:${port} <<EOF
         "members": [
             {
                 "_id": 0,
-                "host": "${externalMongo}:${port}"
+                "host": "${mongodb1}:${port}"
             }
         ]
     };
